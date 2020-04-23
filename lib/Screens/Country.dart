da56d235d4809473b9f565f7681326b6c5951890
ajit57 <ajitdas2900@gmail.com>
2020-04-23 16:41:45 +0600

    first commit

---
+++ lib/Screens/Country.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Country extends StatelessWidget {
+  final Map country;
+  Country(this.country);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pink,
+        title: Text(country['name']),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:countryfinder/Screens/AllCountries.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: AllCountries(),
+  ));
+}
